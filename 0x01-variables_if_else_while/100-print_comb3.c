a71c31d42602a8cba4085f38cd9ec09c57a2e47f
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-09 04:19:54 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - the main method
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int b;
+	int e;
+
+	for (b = 0 ; b < 10 ; b++)
+	{
+		for (e = 0 ; e < 10 ; e++)
+		{
+			if (!((b == e) && (b > e)))
+			{
+				putchar(b);
+				putchar(e);
+
+				if (!((b == 8) && (e == 9)))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
