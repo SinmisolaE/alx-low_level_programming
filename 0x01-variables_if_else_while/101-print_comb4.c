ce0c333e94985cd69290c9c930e69cffc4c5e19a
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-09 06:07:11 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - the main method
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		for (b = 0 ; b < 10 ; b++)
+		{
+			for (c = 0 ; c < 10 ; c++)
+			{
+				if (!((a == b) || (a > b) || (b == c) || (b > c) || (a == c) || (a > c)))
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (!((a == 7) && (b == 8) && (c == 9)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+}
