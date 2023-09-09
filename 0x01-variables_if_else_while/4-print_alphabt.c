76f86580ef757f98972001464d93d421ec44c34a
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-09 01:31:53 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,8 @@ int main(void)
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-		if (i == 'q' || i == 'e')
-			;
-		else
+		if (i != 'q' && i != 'e')
 			putchar(i);
-	;
 	putchar('\n');
+	return (0);
 }
