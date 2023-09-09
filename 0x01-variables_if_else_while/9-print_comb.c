22eb53fca84b40eae85afbac095c4e1afef928db
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-09 03:38:45 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,14 @@
 int main(void)
 {
 	int i;
-	int a;
 
 	for (i = 0 ; i < 10 ; i++)
-		for (a = 0 ; a < 10 ; a++)
-			putchar(i + '0');
-			putchar(a + '0');
-			putchar(', ');
-	putchar('\n');
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('/n');
 	return (0);
 }
