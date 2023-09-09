1a66a832ad7aaa77ac4f8be74e5ac9c490408d2e
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-08 17:48:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - the main method
+ *
+ * Return: returns 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int ch;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	 ch = n % 10;
+
+	 if (ch > 5)
+		 printf("Last digit of %d is %d and is greater than 5", n, ch);
+	 else if (ch == 0)
+		 printf("Last digit of %d is %d and is 0", n, ch);
+	 else if (ch < 6 && ch != 0)
+		 printf("Last digit of %d is %d and is less than 6 and not 0", n, ch);
+	 printf("\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main method
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	for (i = 'a' : i <= 'z' : i++)
+		putchar(i);
+	putchar("\n");
+}
