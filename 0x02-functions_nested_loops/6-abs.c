29235eab35c5acce6e844c55b587490fa580638c
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-12 15:42:55 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ *
+ * @int: the character to be checked
+ * Return: returns 0
+ */
+
+int _abs(int)
+{
+	int j;
+
+	if (j < 0)
+		return (-1 * j);
+	else
+		return (j);
+}
