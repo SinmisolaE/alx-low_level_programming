0e8e636a377870beb837ea42de85bee2f49679b2
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-12 16:40:12 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * times_table - create a times table of the first 9 numbers
+ *
+ * Return: returns nothing
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j < 10; j++)
+			_putchar((i * j) + '0');
+	}
+}
