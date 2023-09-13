dc054982b65993ee12b262d582ad2fbe230db75b
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-12 18:13:07 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: the variable to be checked
+ * Return: returns nothing
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		if (n > 9)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		else
+			_putchar(n + '0');
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n++;
+	}
+}
