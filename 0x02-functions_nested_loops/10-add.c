ff9cde543326d136feb63caebbafee446211658d
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-12 18:00:55 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ *
+ * @x: first value to be checked
+ * @y: second value
+ * Return: returns result
+ */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
