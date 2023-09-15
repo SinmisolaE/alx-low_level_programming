eee00c257647a823349ed59ac6b0d126a1bca3b3
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-14 17:34:22 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - the method
+ * @a: the first variables to be checked
+ * @b: other variable
+ * Return: return result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
