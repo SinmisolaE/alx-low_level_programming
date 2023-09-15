328816ba792fd82b51dce4a3dc8bf32cb1971d15
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-14 19:05:30 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main method
+ * Return: returns value
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 15)
+			printf("FizzBuzz ");
+		else if (i == 100)
+			printf("Buzz");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+}
