42af478ec9559a6b974cae1b0fcbd7e80b6732e3
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-14 18:39:20 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,10 +7,24 @@
 
 void print_diagonal(int n)
 {
-	while (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		_putchar('\');
-		n--;
+		int i;
+		int j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
+
 }
