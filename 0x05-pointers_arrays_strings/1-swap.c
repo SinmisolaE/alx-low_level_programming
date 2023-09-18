ce71865fe6428df8748813fa44e78981ea5bf71c
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-18 13:51:00 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two value
+ * @a: value to be checked
+ * @b: second value to be checked
+ * Return: returns nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int ch;
+
+	ch = a;
+	a = b;
+	b = a;
+}
