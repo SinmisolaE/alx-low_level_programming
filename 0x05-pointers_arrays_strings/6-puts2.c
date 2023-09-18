d713a39128a0abfd59280e848415393be8d0ce22
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-18 16:39:22 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,15 @@ void puts2(char *str)
 	int count = 0;
 	int i;
 	char *y = str;
+	int t = 0;
 
 	while (*y != '\0')
 	{
 		count++;
 		y++;
 	}
-	for (i = 0; i < count; i++)
+	t = count - 1;
+	for (i = 0; i <= t; i++)
 	{
 		if (i % 2 == 0)
 			_putchar(str[0]);
