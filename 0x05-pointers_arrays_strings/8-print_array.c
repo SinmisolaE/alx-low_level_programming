bc77be3c9e6814e03316099ed55e74bec820f8d8
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-18 17:00:18 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_array - print n elements
+ * @a: variable first
+ * @n: sec variable
+ * Return: returns nain
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+		printf("%d, ", a[i]);
+	if (i == (n - 1))
+		printf("%d, ", a[n - 1]);
+	printf("\n");
+}
