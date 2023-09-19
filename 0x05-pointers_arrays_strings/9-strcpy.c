120604b58be9041c83a00d05a33fa44f640c67fd
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-18 17:15:13 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,4 +6,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+	int l = 0;
+	int r = 0;
+
+	while (*(src + l) != '\0')
+		l++;
+	for (; x < l; x++)
+		dest[x] = src[x];
+	dest[l] = '\0';
+	return (dest);
 }
