ae0a612136d6626b5081af283c982b3b83c65b5f
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-21 06:32:52 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encode string using rot13
+ * @s: var
+ * Return: s
+ */
+
+char *rot13(char *s)
+{
+	int i = 0;
+	int j;
+	char f1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char f2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == f1[j])
+				s[i] = f2[j];
+		}
+		i++;
+	}
+	return (s);
+}
