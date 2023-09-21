b5bf43fcf0efe8a8bbd5f6679921592859aafeb1
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-21 05:47:18 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,20 +7,19 @@
 
 char *leet(char *s)
 {
+	int j;
 	int i = 0;
+	char f1 = "aAeEoOtTlL";
+	char f2 = "4433007711";
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == f1[j])
+				s[i] = f2[j];
+			j++;
+		}
 		i++;
 	}
 	return (s);
