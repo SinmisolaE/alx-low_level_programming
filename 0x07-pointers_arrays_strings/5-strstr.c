fbe7d8a168202a84d6b5b593adf616c33ff24b33
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-26 13:43:29 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,23 +7,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-
-	if (*needle == 0)
-		return (haystack);
-	while (*haystack)
+	for (; *haystack != '\0'; haystack++)
 	{
-		i = 0;
+		char *i = haystack;
+		char *p = needle;
 
-		if (haystack[i] == needle[i])
+		while (*i == *p && *p != '\0')
 		{
-			do {
-				if (needle[i + 1] == '\0')
-					return (haystack);
-				i++;
-			} while (haystack[i] == needle[i];
+			i++;
+			p++;
 		}
-		haystack++;
+		if (*p == '\0')
+			return (haystack);
 	}
-	return ('\0');
+	return (0);
 }
