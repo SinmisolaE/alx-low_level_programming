7c6b1b8f18756c7c90c3886ee401c6c57772b00e
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-26 07:12:39 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - seraches a string
+ * @s: checks for char
+ * @accept: from this var
+ * Return: returns the pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int count = 0;
+
+	while (accept(count) != '\0')
+		count++;
+
+	while (*s)
+	{
+		for (i = 0; i < count; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
