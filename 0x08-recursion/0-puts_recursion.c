bd273aeb33a7c23139654c73e6c6daf0f549add8
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-28 06:17:32 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,9 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_putchar(*s++);
-		_puts_recursion(char *s);
+		_puts_recursion(*s);
 	}
-	_putchar('\0');
-	_putchar('\n');
 }
