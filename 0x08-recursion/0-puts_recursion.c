5ad4d2b3097c7bfbc97732a7c7cd3f3b4305c360
SinmisolaE <sinmisolaajao@gmail.com>
2023-09-28 05:39:57 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string using recursion
+ * @s: the string
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(char *s);
+	}
+}
