f2421f5e37d1e3f11ad50a6726b54f6c9ee2db79
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-02 18:31:54 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main method
+ * @argc: num arg
+ * @argv: values
+ * Return: nain
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
