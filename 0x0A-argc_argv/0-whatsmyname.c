1d329beddfcf2e5df2d6f341f635c5783ab8472e
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-02 17:41:45 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the main method
+ * @argc: the number of aregument
+ * @argv: array arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
