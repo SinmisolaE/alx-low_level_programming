cdea6de2a5babd11686c950654c23e6c5047f3aa
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-03 06:58:30 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,11 +9,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *t;
+	unsigned int i;
 
-	if (size == 0)
-		Return('\0');
-
-	t = (int *)malloc(size * sizeof(char));
+	t = malloc(size * sizeof(char));
+	if (size == 0 || t == NULL)
+		return (NULL);
+	for (i = 0 i < size; i++)
+		t[i] = c;
 	return (t);
-
 }
