78cfcde9fd98778f6d143453c500cb5acc5298f4
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-03 06:31:13 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * create_array - create array of chars
+ * @size: size
+ * @c: the char
+ * Return: Returns pointer, Null if error
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *t;
+
+	if (size == 0)
+		Return('\0');
+
+	t = malloc(size * sizeof(char));
+	return (t);
+
+}
