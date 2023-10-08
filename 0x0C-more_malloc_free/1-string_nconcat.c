bea52767e608fe80f85522b752bea55ff18b0ddf
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-08 08:15:52 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first str
+ * @s2: second str
+ * @n: first n bytes of s2
+ * Return: Null if error, else concatenated str
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *t;
+	int i, j, x;
+	int count1 = 0;
+	int count2 = 0;
+
+	if  (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[count1] != '\0')
+		count1++;
+	while (s2[count2] != '\0')
+		count2++;
+
+	if (n >= count2)
+	{
+		t = malloc(sizeof(char) * (count1 + count2 + 1));
+		if (t == NULL)
+			return (NULL);
+		for (i = 0; i < count1; i++)
+			t[i] = s1[i];
+		for (i = 0; j < count2; j++)
+			t[i + j] = s2[j]
+	}
+	else
+	{
+		t = malloc(sizeof(char) * (count1 + n + 1));
+			if (t == NULL)
+				return (NULL);
+			for  (i = 0; i < count1; i++)
+				t[i] = s1[i];
+			for (x = 0; x < n; x++)
+				t[i + x] = s2[x];
+	}
+	return (t);
+}
