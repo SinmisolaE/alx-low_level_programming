d857932b64656a98f1c4b9f421a5ea068498eb05
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-08 16:53:47 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,6 +10,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr;
+	char *first_ptr;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -24,16 +25,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
+
+	first_ptr = ptr;
+
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			new_ptr[i] = ptr[i];
+			new_ptr[i] = first_ptr[i];
 	}
 	if (old_size > new_size)
 	{
 		for (i = 0; i < new_size; i++)
-			new_ptr[i] = prt[i];
+			new_ptr[i] = first_ptr[i];
 	}
-	free(ptr);
+	free(first_ptr);
 	return (new_ptr);
 }
