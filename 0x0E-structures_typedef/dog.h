2470a116fb89261379ad7f289314fc7c26b57875
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-10 15:28:18 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for a dog
+ * @name: name of daog
+ * @age: it's age
+ * @owner: it's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
