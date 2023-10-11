805bd13bfb93e44d74bcf51c6809267ad794ad06
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-11 10:22:36 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "dog.h"
 
-int _strlen (char *word);
+int _strlen(char *word);
 void _strcpy(char *fir, char *sec);
 
 /**
@@ -55,7 +55,7 @@ dog_t *new_dog(char *name, float age, char *owner)
  * @word: the word passed
  * Return: length
  */
-int _strlen (char *word)
+int _strlen(char *word)
 {
 	int i = 0;
 
