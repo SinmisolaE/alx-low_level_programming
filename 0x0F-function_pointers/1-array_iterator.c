df35b10cb37a21e24c8e80f5915dac159bdeb475
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-11 21:41:04 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - func that executes a func on an arry
+ * @array: the array
+ * @size: size of array
+ * @action: func being passed
+ * Return: nain
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
