1b6bc8cc9aabeae093dd4f3face847fbf1d31ad3
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-12 21:13:11 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,9 +14,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list t;
 
 	va_start(t, n);
+
 	for (i = 0; i < n; i++)
 	{
-		 s = va_arg(t, char *);
+		s = va_arg(t, char *);
 		if (s  == NULL)
 			printf("(nil)");
 		else
