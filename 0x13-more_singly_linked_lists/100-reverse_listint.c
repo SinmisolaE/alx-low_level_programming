8d17a551d95994130864831a494c8b0e38a21252
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-25 07:12:57 -0700

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse a list
+ * @head: the list
+ * Return: 1st  node of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next = NULL;
+	listint_t *previous = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next;
+	}
+	return (previous);
+}
