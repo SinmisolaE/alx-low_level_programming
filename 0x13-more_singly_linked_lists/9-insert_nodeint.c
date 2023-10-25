2409387f5cc76a3eb94b7ef30eec3187a65e63c8
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-24 20:39:18 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
-	if (!*head)
+	if (!*head && idx > 0)
 		return (NULL);
 
 	if (idx == 0)
