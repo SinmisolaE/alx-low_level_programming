126c689251ce1d166ba284cafb2a3c6380d78fa2
SinmisolaE <sinmisolaajao@gmail.com>
2023-10-27 17:15:51 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - gets endianness
+ * Return: 1 if litttle endianness, else 0
+ */
+int get_endianness(void)
+{
+	int num = 1;
+
+	char *ptr = (char*)&num;
+
+	if (*ptr == 1)
+		return (1);
+	else
+		return (0);
+}
