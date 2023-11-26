c878f7fbad2ec4a2caf108fccf125e5d81608839
SinmisolaE <sinmisolaajao@gmail.com>
2023-11-26 06:35:20 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,37 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - prints time table
+ * @n: the number
+ * Return: nain
+ */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-	{
+	int i, j, a;
 
-	}
-	else
+	if (n >= 0 && n <= 15)
 	{
-		_putchar("tijan");
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				a = i * j;
+
+				if (a > 9)
+				{
+					_putchar((a / 10) + '0');
+					_putchar((a % 10) + '0');
+				}
+				else
+					_putchar(a + '0');
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+					_putchar('\n');
+			}
+		}
 	}
 }
