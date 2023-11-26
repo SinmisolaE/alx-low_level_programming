496aa2d631e917e97fdfb032b5527f15c30c1c1f
SinmisolaE <sinmisolaajao@gmail.com>
2023-11-26 12:54:10 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,19 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * multiple - prints multiples
+ * main - prints multiples
+ * Return: 0
  */
-void multiple(void)
+int main(void)
 {
 	int i;
 	int sum = 0;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || (i % 5 == 0))
 			sum += i;
 	}
 	printf("%d\n", sum);
+	return (0);
 }
