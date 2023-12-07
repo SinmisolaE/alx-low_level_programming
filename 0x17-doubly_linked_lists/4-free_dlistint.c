43f59a8dbeaa4cada007c6b2c6331b8f65ec63f1
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-07 07:41:49 -0800

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - free list
+ * @head: head of list
+ * Return: nain
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *curr;
+	dlistint_t *next;
+	curr = head;
+
+	while (curr)
+	{
+		next = curr->next;
+		free(curr->prev);
+		free(curr->next);
+		curr = next;
+	}
+	free(curr);
+}
