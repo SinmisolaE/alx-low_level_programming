d1e26ae2905d2774fc42c06d26dde00395c8c603
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-08 07:26:34 -0800

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,6 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->next = (*h)->next;
 		new->prev = NULL;
+		*(h)->prev = new;
 		*h = new;
 		return (*h);
 	}
