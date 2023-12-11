811e8fdc395bf994197fbbfcffbd872f0f5b67d1
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-11 12:31:11 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * div - divides two int
+ * @a: first int
+ * @b: ssecond int
+ * Return: division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * mod - modulos of two int
+ * @a: first int
+ * @b: second int
+ * Return: the modulos
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
