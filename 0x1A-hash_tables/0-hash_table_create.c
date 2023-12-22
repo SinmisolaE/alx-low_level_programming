0ee18bc77e95eb79b6d360644406600224a6e09a
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-22 06:34:10 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new->size = size;
 	new->array = malloc(sizeof(hash_node_t) * size);
-	if (!new->array)
+	if (!(new->array))
 		return (NULL);
 
 	for (i = 0; i < size; i++)
